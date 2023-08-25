5c7d2db4eed335b3b92edca393af5bf468236eef
unknown <harshal011kamble@gmail.com>
2023-08-25 20:44:28 +0530

    done scene

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import  counterSlice  from './slices/counter';
-import  barringSlice  from './slices/barring';
 export const store = configureStore({
     reducer:{
-        counter:counterSlice,
-        bare:barringSlice
+        counter:counterSlice
     }
 });
